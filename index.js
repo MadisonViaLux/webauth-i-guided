a48c26718d4fba65fba5c55197b33492175ca8b2
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-14 10:56:26 -0700

    10/14/2019 v2

--- index.js
+++ index.js
@@ -16,14 +16,28 @@ server.get('/', (req, res) => {
   res.send("It's alive!");
 });
 
+
+
 server.get('/hash', (req, res) => {
+  const password = req.headers.authorization
   // read a password from the Authorization header
-  // return an object with the password hashed using bcryptjs
-  // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
+  if(password){
+
+    const hash = bcrypt.hashSync(password, 8)
+    // return an object with the password hashed using bcryptjs
+    
+    res.status(200).json({hash})
+    // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
+  } else {
+    res.status(400).json({message: 'fix you stuff boi'})
+  }
 })
+
+
 
 server.post('/api/register', (req, res) => {
   let user = req.body;
+
 
   const hash = bcrypt.hashSync(user.password, 12)
 
@@ -38,30 +52,51 @@ server.post('/api/register', (req, res) => {
     });
 });
 
+
+
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+  if(username && password){
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'YOU SHALL NOT PASS' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+    } else {
+      res.status(400).json({message: 'fix you stuff boi'})
+    }
 });
 
-server.get('/api/users', (req, res) => {
+
+
+server.get('/api/users', protected, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+
+
+function protected(req){
+  const {username, password} = req.headers
+
+  if(username && password){
+    next()
+    // I forgot how to write middlewear XD
+  }
+}
+
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
